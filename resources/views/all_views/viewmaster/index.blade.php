8f6a730086ec7c070565158180d353077a8e9715
GianlucaJC <morescogianluca@gmail.com>
2022-09-24 11:30:41 +0200

    fix footer

--- resources/views/all_views/viewmaster/index.blade.php
+++ resources/views/all_views/viewmaster/index.blade.php
@@ -180,7 +180,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; <?php echo date("Y"); ?>
-	<a href="http://www.gruppoingenious.com/">Gruppo INGENIOUIS </a>.</strong>
+	<a href="http://www.gruppoingenious.com/">Gruppo INGENIOUS </a>.</strong>
   </footer>
 </div>
 <!-- ./wrapper -->
