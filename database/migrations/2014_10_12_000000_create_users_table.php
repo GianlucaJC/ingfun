b3d958b49b3967c71e257928f5116d2aea542848
GianlucaJC <morescogianluca@gmail.com>
2022-09-27 12:36:33 +0200

    UPdate migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        
+        /*
 		Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+		*/
 		
     }
 
