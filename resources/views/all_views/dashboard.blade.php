e9d308d977b4ee5fc66c660ee7eb84f98b031844
GianlucaJC <morescogianluca@gmail.com>
2022-10-07 10:18:07 +0200

    Edit from CH

--- resources/views/all_views/dashboard.blade.php
+++ resources/views/all_views/dashboard.blade.php
@@ -16,7 +16,7 @@ use App\Models\User;
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Divisione FUNEBRE | Servizi in primo piano</h1>
+            <h1 class="m-0">Divisione FUNEBRE | Servizi in primo pianox</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
