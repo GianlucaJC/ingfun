2ee0aa7e1334cb8bd0063de8dfc9e8c333108005
GianlucaJC <morescogianluca@gmail.com>
2023-10-31 15:58:35 +0100

    update sinistri

--- public/dist/js/sinistri.js
+++ public/dist/js/sinistri.js
@@ -1,3 +1,24 @@
+	var video, canvas, ctx;
+	if(navigator && navigator.mediaDevices){
+		const options = { audio: false, video: { facingMode: "user", width: 300, height: 300  } }
+		navigator.mediaDevices.getUserMedia(options)
+		.then(function(stream) {
+			video = document.querySelector('video');
+			video.srcObject = stream;
+			video.onloadedmetadata = function(e) {
+			  video.play();
+			};
+			canvas = document.getElementById("myCanvas");
+			ctx = canvas.getContext('2d');
+		})
+		.catch(function(err) {
+			//Handle error here
+		});
+	}else{
+		console.log("camera API is not supported by your browser")
+	}
+
+
 (function () {
   'use strict'
 
@@ -26,25 +47,6 @@
     })
 })()
 $(document).ready( function () {
-	var video, canvas, ctx;
-	if(navigator && navigator.mediaDevices){
-		const options = { audio: false, video: { facingMode: "user", width: 300, height: 300  } }
-		navigator.mediaDevices.getUserMedia(options)
-		.then(function(stream) {
-			video = document.querySelector('video');
-			video.srcObject = stream;
-			video.onloadedmetadata = function(e) {
-			  video.play();
-			};
-			canvas = document.getElementById("myCanvas");
-			ctx = canvas.getContext('2d');
-		})
-		.catch(function(err) {
-			//Handle error here
-		});
-	}else{
-		console.log("camera API is not supported by your browser")
-	}
 
 
 } );
