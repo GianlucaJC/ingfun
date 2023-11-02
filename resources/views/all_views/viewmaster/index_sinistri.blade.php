c58cdc4f7efbe3b96d9d491182d7cfd03156ced0
GianlucaJC <morescogianluca@gmail.com>
2023-11-02 11:45:30 +0100

    Update sinistri

--- resources/views/all_views/viewmaster/index_sinistri.blade.php
+++ resources/views/all_views/viewmaster/index_sinistri.blade.php
@@ -27,9 +27,9 @@
 <div class="wrapper">
 
   <!-- Navbar -->
-  <nav class="main-header   navbar navbar-expand navbar-white navbar-light">
+  <nav class=" main-header   navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
-    <ul class="navbar-nav">
+    <ul class="navbar-nav  d-none d-sm-inline-block">
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
