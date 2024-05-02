f5caa80ebab93d0148b95bd9b9f9854438a7d438
GianlucaJC <morescogianluca@gmail.com>
2024-05-02 15:47:17 +0200

    Update misapp web

--- resources/views/all_views/misapp/misapp.blade.php
+++ resources/views/all_views/misapp/misapp.blade.php
@@ -14,6 +14,16 @@
 
 @section('extra_style') 
 	<script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js"></script>
+
+	<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+	<script>
+	window.OneSignalDeferred = window.OneSignalDeferred || [];
+	OneSignalDeferred.push(function(OneSignal) {
+		OneSignal.init({
+		appId: "f9677f83-05dd-44ed-b301-b5c49d5c8777",
+		});
+	});
+	</script>	
 @endsection
 
 
