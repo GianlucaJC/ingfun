6c76883a4d1d5959e25a8e78a8df915f0b6d2a74
GianlucaJC <morescogianluca@gmail.com>
2024-05-10 11:32:20 +0200

    first release rifornimenti bis

--- resources/views/all_views/viewmaster/index.blade.php
+++ resources/views/all_views/viewmaster/index.blade.php
@@ -60,6 +60,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="theme-color" content="#dc3545"/>
+
   <title>@yield('title')</title>
 
   <!-- Google Font: Source Sans Pro -->
