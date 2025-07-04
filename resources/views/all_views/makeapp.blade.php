1572132deec57d3d8d0abb9d759a78dae744fa4e
GianlucaJC <morescogianluca@gmail.com>
2025-07-04 11:58:04 +0200

    fix modal close top

--- resources/views/all_views/makeapp.blade.php
+++ resources/views/all_views/makeapp.blade.php
@@ -248,7 +248,7 @@ th, td {
             <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="modalinfotitle">Informazioni sull'appalto</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
